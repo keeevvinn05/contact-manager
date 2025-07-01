1f6924459ab174731e21624ec3d274144f2457d7
keeevvinn05 <barrosok526@gmail.com>
2025-07-01 11:31:14 +0200

    set up primeNG and primeIcons

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -3,6 +3,19 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura';
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        preset: Aura
+      }
+    }), // ← COMA necesaria aquí
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes)
+  ]
 };
