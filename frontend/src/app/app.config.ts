e4f4d295134df0308eb9dad57f67587b11f32999
keeevvinn05 <barrosok526@gmail.com>
2025-07-02 09:07:05 +0200

    guardar todo

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -2,15 +2,18 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
+
 
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
-import Aura from '@primeng/themes/aura';
+
 import { MyPreset } from '../theme/my-presset';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideAnimationsAsync(),
+    provideHttpClient(),
     providePrimeNG({
       theme: {
         preset: MyPreset
